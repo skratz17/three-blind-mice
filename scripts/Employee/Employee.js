b6c029c0056fa8e370a64306cd1b957491ec87f4
Jacob Eckert <jweckert17@gmail.com>
2020-08-21 09:51:20 -0500

    implemented location stuff

--- scripts/Employee/Employee.js
+++ scripts/Employee/Employee.js
@@ -1,5 +1,5 @@
 export const Employee = employee => {
-  const { id, firstName, lastName, age, computer, department } = employee;
+  const { id, firstName, lastName, age, computer, department, location } = employee;
 
   return `
     <section id="employee--${id}" class="employee card">
@@ -18,6 +18,10 @@ export const Employee = employee => {
       <div class="card__content-group">
         <p class="card__label">Department</p>
         <p class="department__info">${department.name}</p>
+      </div>
+      <div class="card__content-group">
+        <p class="card__label">Location</p>
+        <p class="location__info">${location.name}</p>
       </div>
     </section>
   `;
